2408cf4502666a91b8a9ab6e210a7c0e9e180cbc
Dant Benjamin <sukkhato@hotmail.com>
2015-11-27 19:00:32 +0100

    -Premier commit du projet Simplon du 27-11-2015

---
+++ 20151127/projet/js/app.js
@@ -0,0 +1,8 @@
+console.log("test");
+var intro = "<p>Texte d'intro</p>";
+var apropos = "<p>Le texte de à propos...</p>";
+var contact = "";
+alert(intro);
+$(document).ready(function(){
+	alert('Ready!');
+});
