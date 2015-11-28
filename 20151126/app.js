f5422459ec8684508f38db0a3ba4e8b256c5bc05
Dant Benjamin <sukkhato@hotmail.com>
2015-11-28 19:33:39 +0100

    -Modification mineure de l'ajax, n'est plus fonctionnel comme auparavant...

--- 20151126/app.js
+++ 20151126/app.js
@@ -1,3 +1,8 @@
+var barNav = ['#identity','#grade','#work','#hobbies'];		//table des éléments du tableau
+var lenNavBar = barNav.length;								//taille du tableau
+var idText = ["<br>Nom</br><br>Prénom</br>","Mon Cursus Scolaire", "Mon travail", "Mes activités préférées"];
+
+//méthode du cour, je ne sais pas quoi en faire pour le moment...
 function comparChaine(str){
 	var maj = str.toUpperCase();
 	if (str == maj){
@@ -6,18 +11,32 @@ function comparChaine(str){
 		document.writeln(str + " : n'est pas qu'en majuscules.</br>")
 	}
 }
+
+//Méthode pour automatiser l'évènement click sur la barre de navigation <nav></nav> ce 
+//qui modifie l'élément #main-content
+function clickElement(element, text, target){
+	var page = element.replace('#','');
+	page = "ajax/"+page+".html";
+	$(element).click(function(){
+		for (i = 0; i < lenNavBar; i++){
+			$(barNav[i]).removeClass("active");		
+		}
+		$(element).addClass("active");
+		$(target).load(page);
+		//$(target).writeln(text);
+	});
+}
+
+//Code principal modifiant l'affichage de la page
 $(document).ready(function(){
-	//var chaine = comparChaine("Bonjour et Bienvenue !");
-	$('#identity').click(function(){
-		$('#main-content').load("ajax/identity.html");
+	/*Ajout de l'évènement click sur les liens de la barre de navigation <nav></nav>
+	ayant pour effet de modifier le contenu de la balise avec l'id #main-content*/
+	for (i = 0; i<lenNavBar; i++){
+		clickElement(barNav[i], idText[i], '#main-content');
+	}
+	$(".link").click(function(){
+		alert('Thank you for visiting the site...');
+		event.preventDefault();
 	});
-	$('#grade').click(function(){
-		$('#main-content').load("ajax/grade.html");
-	});
-	$('#work').click(function(){
-		$('#main-content').load("ajax/work.html");
-	});
-	$('#hobbies').click(function(){
-		$('#main-content').load("ajax/hobbies.html");
-	});
+	
 });
