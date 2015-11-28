33d3d8dba863a4d2b593098db780f38f5dd1ca14
Dant Benjamin <sukkhato@hotmail.com>
2015-11-28 19:36:02 +0100

    -Modification du projet en groupe avec Raphael, Thomas et Yoann

--- 20151127/projet/js/app.js
+++ 20151127/projet/js/app.js
@@ -1,8 +1,46 @@
-console.log("test");
+//JavaScript
+"use strict";
+$(document).ready(function(){
+
+console.log("jQuery begin");
+
 var intro = "<p>Texte d'intro</p>";
 var apropos = "<p>Le texte de à propos...</p>";
 var contact = "";
-alert(intro);
-$(document).ready(function(){
-	alert('Ready!');
+
+var button = document.getElementById("hide"),
+	target = document.getElementById("contenu");
+
+function hide(){
+	if (target.hasClass("hide")){
+		target.removeClass("hide");
+	}else{
+		target.setAttribute("class", "hide");	
+	}
+}
+
+button.addEventListener("click", hide, false);
+
+
+console.log('jQuery end');
+
 });
+/*switch (){
+	case "";
+		break;
+	default:
+}
+document.getElementById()
+.getElementByTagName()
+.getElementByClassName()
+.querySelector()
+.querySelectorAll()
+*/
+
+//jQuery
+/*$(document).ready(function(){
+	$('').innerHTML = "Petit aperçu de ce que jQuery va faire.";
+
+
+	console.log('jQuery Ready!');
+});*/
