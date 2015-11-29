e1fb69c898742f77bb3e7eea3a3b20041284cd25
Dant Benjamin <sukkhato@hotmail.com>
2015-11-29 10:28:02 +0100

    Mise à Jour: -Ajout d'un bouton qui cache du texte (pour le plaisir) -Ajout d'une barre de navigation fonctionnelle

--- 20151127/projet/js/app.js
+++ 20151127/projet/js/app.js
@@ -1,41 +1,62 @@
 //JavaScript
 "use strict";
-$(document).ready(function(){
+$(document).ready(function(){	//jQuery utilisé ici pour être sûr que le Javascript démarre quand tous les éléments de la page sont prêts.
 
-console.log("jQuery begin");
+console.log("jQuery begin");	//Avertissement console de démarrage
 
-var intro = "<p>Texte d'intro</p>";
-var apropos = "<p>Le texte de à propos...</p>";
-var contact = "";
+//Déclaration des variables
+	//Variables de contenu de page
+var content = ["<p>Le texte de à propos...</p>", "<p>Le texte de contact</p>"];
 
+	//Variables liées à la barre de navigation
+var navBar = document.getElementsByTagName("li");
+var navLen = navBar.length;
+console.log(navBar);		//Avertissement console, contenu de navBar
+console.log(navLen);		//Avertissement console, contenu de navLen
+
+//Attribution des balises par les id dans des variables
 var button = document.getElementById("hide"),
 	target = document.getElementById("contenu");
 
-function hide(){
-	if (target.hasClass("hide")){
-		target.removeClass("hide");
+//fonction qui cache et découvre un élément
+function hide(classe, attribut){
+	if (target.hasAttribute(classe, attribut)){
+		target.removeAttribute(classe, attribut);
 	}else{
-		target.setAttribute("class", "hide");	
+		target.setAttribute(classe, attribut);	
 	}
+};
+button.addEventListener("click", function(){hide("class", "hide")}, false);
+
+//fonction de modification de l'intérieur d'une balise ciblée
+function modifTarget(target, text){
+	document.getElementById(target).innerHTML = text;
+	console.log(text);
+};
+//fonction rendant un élément clickable
+function addNavEvent(target, index){
+	navBar[index].addEventListener("click", function(){modifTarget(target, content[index])});
+}
+//inclusion de l'évènemnt à la barre de navigation
+for (var i = 0; i < navLen; i++){
+	addNavEvent("target", i);
 }
 
-button.addEventListener("click", hide, false);
-
+//création d'un paragraphe
+var targetArea = document.getElementById("target-area");
+var p = document.createElement("div");
+p.innerHTML = "Texte généré par JavaScript";
+targetArea.appendChild(p);
+//modification du texte à l'intérieur de la balise d'id target
+document.getElementById("target").innerHTML = "<p>Paragraphe inclu en JavaScript</p>";
+//création d'un paragraphe d'une autre manière, moins automatisée
+document.getElementsByTagName("footer").item(0).innerHTML = '<div id="java-content"></div>';
+document.getElementById("java-content").innerHTML = "<p>Paragraphe inclu dans un div créés par JavaScript</p>";
 
 console.log('jQuery end');
 
 });
-/*switch (){
-	case "";
-		break;
-	default:
-}
-document.getElementById()
-.getElementByTagName()
-.getElementByClassName()
-.querySelector()
-.querySelectorAll()
-*/
+
 
 //jQuery
 /*$(document).ready(function(){
