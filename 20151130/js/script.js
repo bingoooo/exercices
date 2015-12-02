4af6f4c910d9360415b426c32d310d1a80b6f53d
Dant Benjamin <sukkhato@gmail.com>
2015-12-02 22:17:05 +0100

    -M à J Tutoriel légèrement modifié

--- 20151130/js/script.js
+++ 20151130/js/script.js
@@ -1,10 +1,12 @@
 "use strict";
 
-console.log('JavaScript Linked');
-var navPrior = ['#html', '#css', '#autre', '#balises', '#attributs', '#special', '#synthaxe', '#selectors', '#pseudoclass', '#boxmodel', '#soc', '#dry'];
-var hiddenContent = navPrior.slice(3, navPrior.length);
-console.log(hiddenContent);
+console.log('JavaScript Linked'); //Vérification de l'acivité JS dans la console
 
+//Déclaration des variables
+	//Tableau contenant les éléments du tableau
+var navPrior = ['#html', '#css', '#autre', '#balises', '#attributs', '#special', '#synthaxe', '#selectors', '#pseudoclass', '#boxmodel', '#soc', '#dry'];
+
+//Méthode rendant un élément clickable et cachant un sous élément dont l'id doit être le même que le premier élément plus '-ss'
 function clickToHide(id){
 	$(id).click(function(){
 		var target = id + '-ss';
@@ -12,20 +14,25 @@ function clickToHide(id){
 	});
 }
 
+//Méthode ajoutant la classe hide à un élément
 function hide(id){
 	$(id).addClass('hide');
 }
 
+//Début du programme
 $(document).ready(function(){
+
+	//Boucle attribuant la classe hide aux éléments ciblés (ici, les éléments que l'on veut cacher)
 	for (var i = 0; i < navPrior.length; i++){
 		var content = navPrior[i] + '-ss';
 		hide(content);
 	};
-	hide('#soc-ss');
+
+	//Boucle attribuant l'évènement click 
 	for (var i = 0; i < navPrior.length; i++){
 		clickToHide(navPrior[i]);
 	};
-//	hide(navPrior[0]);
 
-	console.log('jquery');
+	console.log('jquery');//Vérification console de l'activité jQuery
 });
+//sudo apt-get install firmware-b43-installer
