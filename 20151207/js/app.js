d8fef420d712d02334978b34e6d4c442d980e535
Dant Benjamin <sukkhato@gmail.com>
2015-12-07 22:00:39 +0100

    -M à J, la partie haute est désormais fonctionnelle

--- 20151207/js/app.js
+++ 20151207/js/app.js
@@ -1,36 +1,52 @@
+"use strict";
+
 console.log('Linked : app.js');
 
 $(document).ready(function(){
 	console.log('Linked : jquery.js');
 
+	//Fonction ajoutant l'évènement qui supprime l'élément de la liste
 	function supprElem(){
-		$('.delete').click(function(){
-			$(this).parent().remove();	
+		$('.delete').click(function(mouse){
+			mouse.preventDefault;
+			$(this).parent().remove();
+			updateItems();	
 		});
 	}
+
+	//Fonction qui ajoute l'évènement lié à un élément coché
 	function checkElem(){
-		$('.todo-checkbox').change(function(){
-			if ($(this).attr('checked')){
-				$(this).parent().find("span").addClass("checked");
+		$('.todo-checkbox').change(function(event){
+			//console.log(this.checked);
+			event.preventDefault;
+			if (this.checked){
+				$(this).parent().find("span").addClass("ischecked");
+				$(this).parent().addClass("done");
 			}
 			else {
-				$(this).parent().find("span").removeClass("checked");
+				$(this).parent().find("span").removeClass("ischecked");
+				$(this).parent().removeClass("done");
 			}
-			//console.log(status);
 		});		
 	}
-	supprElem();
-	checkElem();
 
+	//Fonction mettant à jour le nombre d'éléments du tableau
+	function updateItems(){
+		$('#status').html($('#todolist').children().size() + " items left");
+	}
+
+	//Gestion nde l'éènement lié à un ajout d'un élément de la Todo list
 	$('#newTodo').keypress(function(key){
 		if (key.which == 13){
 			key.preventDefault();
 			var text = $('#newTodo').val();
-			$('#todolist').append('<div><input type="checkbox" class="todo-checkbox" {checked}><span class="todo-content" tabindex="0">' + text + '</span><button class="delete">X</button></div>');
+			$('#todolist').append('<div class="todo"><input type="checkbox" class="todo-checkbox" {checked}><span class="todo-content">' + text + '</span><button class="delete">X</button></div>');
+			updateItems();
 			console.log(text);
-			$('button').preventDefault();
 			supprElem();
 			checkElem();
 		}
 	});
+
+	
 })
